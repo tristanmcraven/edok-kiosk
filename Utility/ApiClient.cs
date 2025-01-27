680617c8aee588804641ada0dd3b12129c67a8bd
Tristan McRaven <tristanmcraven@gmail.com>
2025-01-27 12:46:51 +0500

    Add project files.

---
+++ Utility/ApiClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace edok_kiosk.Utility
+{
+    public static class ApiClient
+    {
+        public static readonly string _apiPath = "http://localhost:5224/api/";
+        public static HttpClient httpClient = new HttpClient();
+
+        
+    }
+}
